81c9907781ddd13ddaf83f56a0a770c25bd6f588
James Reeves <jreeves@weavejester.com>
2011-06-04 13:09:53 +0100

    Echo back clojure data

--- src/cljd/server.clj
+++ src/cljd/server.clj
@@ -1,12 +1,19 @@
 (ns cljd.server
-  (:use [net.tcp.server :only (tcp-server wrap-io start)]))
+  (:use [net.tcp.server :only (tcp-server wrap-io start)])
+  (:import java.io.PushbackReader))
 
-(defn handler [reader writer]
-  (.append writer "Hello World"))
+(defn wrap-clojure [handler]
+  (wrap-io
+   (fn [reader writer]
+     (binding [*in* (PushbackReader. reader), *out* writer]
+       (handler)))))
+
+(defn handler []
+  (prn (read)))
 
 (def server
   (tcp-server
-   :handler (wrap-io handler)
+   :handler (wrap-clojure handler)
    :host "127.0.0.1"
    :port 8000))
 
